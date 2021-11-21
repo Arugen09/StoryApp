5f35942bc0c60bb52ac538d52fcc9846f17948cd
Arugen09 <75859120+Arugen09@users.noreply.github.com>
2021-11-21 12:20:27 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB1to603NjUTlrklg6h6OAHweM4uezWW5Y",
+    authDomain: "storytellingapp-b594d.firebaseapp.com",
+    databaseURL: "https://storytellingapp-b594d-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-b594d",
+    storageBucket: "storytellingapp-b594d.appspot.com",
+    messagingSenderId: "878814905298",
+    appId: "1:878814905298:web:34ed7a827f2c3a15817e0b"
+  };
---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import StackNavigator from "../navigation/StackNavigator";
+import Profile from "../screens/Profile";
+import LogOut from "../screens/LogoutScreen";
+import firebase from "firebase";
+import CustomSideBar from '../screens/CustomSideBar'
+const Drawer = createDrawerNavigator();
+
+export default class DrawerNavigator extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      light_theme: true
+    }
+  }
+  componentDidMount() {
+    let theme
+    firebase.database().ref("/users/" + firebase.auth().currentUser.uid).on('value', function(snapshot){
+      theme = snapshot.val().current_theme
+    })
+    this.setState({
+      light_theme: theme==='light' ? true : false
+    })
+  }
+  render() {
+    let props = this.props
+    return(
+      <Drawer.Navigator
+      drawerContentOptions = {{
+        activeTintColor: 'blue',
+        inactiveTintColor: this.state.light_theme ? 'black' : 'white'
+      }}
+      drawerContent = {
+        <CustomSideBar {...props}/>
+      }>
+        <Drawer.Screen name="Home" component={StackNavigator} options = {{unmountOnBlur: true}} />
+        <Drawer.Screen name="Profile" component={Profile} options = {{unmountOnBlur: true}}/>
+        <Drawer.Screen name="Logout" component={LogOut} options = {{unmountOnBlur: true}}/> 
+      </Drawer.Navigator>
+    )
+  }
+};
